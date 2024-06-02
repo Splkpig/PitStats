12634ffd2bcb9571dd258f40b590fb619410d9d6
Splkpig <splke4pack@gmail.com>
2024-06-02 01:05:44 -0700

    added compare command and finished overview command

--- commands/stats.py
+++ commands/stats.py
@@ -32,7 +32,56 @@ class compare(commands.Cog):
             await interaction.response.send_message(embed=embedFail, ephemeral=True)  # noqa
 
         else:
-            print("your mom lol")
+            prestige = len(player1Data["data"]["prestiges"]) - 1
+            level = pit_functions.xpToLevel(prestige, int(player1Data["data"]["xpProgress"]["displayCurrent"]))
+            xp = player1Data["data"]["doc"]["xp"]
+            gold = player1Data["data"]["doc"]["lifetimeGold"]
+            kills = player1Data["data"]["doc"]["kills"]
+            deaths = player1Data["data"]["doc"]["deaths"]
+            kdr = str(player1Data["data"]["doc"]["kdr"])
+            kdr = kdr[:kdr.index(".") + 3]
+            timeplayed = player1Data["data"]["doc"]["playtime"]
+
+            player1DataList = [prestige, level, xp, gold, kills, deaths, kdr, timeplayed]
+
+            prestige = len(player2Data["data"]["prestiges"]) - 1
+            level = pit_functions.xpToLevel(prestige, int(player2Data["data"]["xpProgress"]["displayCurrent"]))
+            xp = player2Data["data"]["doc"]["xp"]
+            gold = player2Data["data"]["doc"]["lifetimeGold"]
+            kills = player2Data["data"]["doc"]["kills"]
+            deaths = player2Data["data"]["doc"]["deaths"]
+            kdr = str(player2Data["data"]["doc"]["kdr"])
+            kdr = kdr[:kdr.index(".") + 3]
+            timeplayed = player2Data["data"]["doc"]["playtime"]
+
+            player2DataList = [prestige, level, xp, gold, kills, deaths, kdr, timeplayed]
+
+            embed = discord.Embed(title=f"[{formatting_functions.int_to_roman(player1DataList[0])}-{player1DataList[1]}] {player1Data['data']['name']} vs [{formatting_functions.int_to_roman(player2DataList[0])}-{player2DataList[1]}] {player2Data['data']['name']}", color=calcBracketColor(max(player1DataList[0], player2DataList[0])))
+
+            signs = []
+
+            for i in range(len(player1DataList)):
+                if player1DataList[i] < player2DataList[i]:
+                    signs.append('<')
+                elif player1DataList[i] > player2DataList[i]:
+                    signs.append('>')
+                else:
+                    signs.append('=')
+
+            embed.add_field(name="Prestige & Level:", value=f"[{formatting_functions.int_to_roman(player1DataList[0])}-{player1DataList[1]}]    {signs[0]}    [{formatting_functions.int_to_roman(player2DataList[0])}-{player2DataList[1]}]", inline=False)
+            embed.add_field(name=" XP Grinded:", value=f"{formatting_functions.add_commas(player1DataList[2])} XP {signs[2]} {formatting_functions.add_commas(player2DataList[2])} XP", inline=False)
+            embed.add_field(name="Gold Grinded:", value=f"{formatting_functions.add_commas(player1DataList[3])} G {signs[3]} {formatting_functions.add_commas(player2DataList[3])} G", inline=False)
+            embed.add_field(name="Kills:", value=f"{formatting_functions.add_commas(player1DataList[4])} {signs[4]} {formatting_functions.add_commas(player2DataList[4])}", inline=False)
+            embed.add_field(name="Deaths:", value=f"{formatting_functions.add_commas(player1DataList[5])} {signs[5]} {formatting_functions.add_commas(player2DataList[5])}", inline=False)
+            embed.add_field(name="KDR:", value=f"{player1DataList[6]} {signs[6]} {player2DataList[6]}", inline=False)
+            embed.add_field(name="Time Played:", value=f"{formatting_functions.format_playtime(int(player1DataList[7]))} {signs[7]} {formatting_functions.format_playtime(int(player2DataList[7]))}", inline=False)
+
+            if player1DataList[0] > player2DataList[0]:
+                embed.set_thumbnail(url=f"https://visage.surgeplay.com/face/512/{player1Data['data']['uuid']}?format=webp")
+            else:
+                embed.set_thumbnail(url=f"https://visage.surgeplay.com/face/512/{player2Data['data']['uuid']}?format=webp")
+
+            await interaction.response.send_message(embed=embed) # noqa
 
     @app_commands.command(name="overview", description='Shows an overview of a player\'s stats')
     async def overview(self, interaction: discord.Interaction, player: str):
@@ -63,13 +112,13 @@ class compare(commands.Cog):
             timeplayed = formatting_functions.format_playtime(int(timeplayed))
 
             embed = discord.Embed(title=f"Player Stats for [{formatting_functions.int_to_roman(prestige)}-{level}] {data['data']['name']}", color=calcBracketColor(int(prestige)))
-            embed.add_field(name="", value=f"Prestige & Level: [{formatting_functions.int_to_roman(prestige)}-{level}]", inline=False)
-            embed.add_field(name="", value=f"<:8983xpbottle:1245974825865056276> XP Grinded: {formatting_functions.add_commas(xp)}", inline=False)
-            embed.add_field(name="", value=f"Gold Grinded: {formatting_functions.add_commas(gold)}", inline=False)
-            embed.add_field(name="", value=f"Kills: {formatting_functions.add_commas(kills)}", inline=False)
-            embed.add_field(name="", value=f"Deaths: {formatting_functions.add_commas(deaths)}", inline=False)
-            embed.add_field(name="", value=f"KDR: {kdr}", inline=False)
-            embed.add_field(name="", value=f"Time Played: {timeplayed}", inline=False)
+            embed.add_field(name="Prestige & Level:", value=f"[{formatting_functions.int_to_roman(prestige)}-{level}]", inline=False)
+            embed.add_field(name="XP Grinded:", value=f"{formatting_functions.add_commas(xp)} XP", inline=False)
+            embed.add_field(name="Gold Grinded:", value=f"{formatting_functions.add_commas(gold)} G", inline=False)
+            embed.add_field(name="Kills:", value=f"{formatting_functions.add_commas(kills)}", inline=False)
+            embed.add_field(name="Deaths:", value=f"{formatting_functions.add_commas(deaths)}", inline=False)
+            embed.add_field(name="KDR:", value=f"{kdr}", inline=False)
+            embed.add_field(name="Time Played", value=f"{timeplayed}", inline=False)
             embed.set_footer(text=footerDateGen())
             embed.set_thumbnail(url=f"https://visage.surgeplay.com/face/512/{data['data']['uuid']}?format=webp")
 
